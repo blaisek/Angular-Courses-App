0d43cd807f920d560bae06b3855954bc22db2aa9
Blaise <blaze.kalonda@gmail.com>
2019-03-18 11:34:55 +0100

    app component and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { StarComponent } from './shared/ui/star/star.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    WelcomeComponent,
+    ProductListComponent,
+    ProductDetailComponent,
+    StarComponent
   ],
   imports: [
     BrowserModule,
