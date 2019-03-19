98e8903a98f242959a42c53d1ab2c630bf276183
Blaise <blaze.kalonda@gmail.com>
2019-03-19 23:51:18 +0100

    search-pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './layout/header/header.component';
 import { WelcomeComponent } from './welcome/welcome.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductListComponent } from './product/product-list/product-list.component';
+import { ProductDetailComponent } from './product/product-detail/product-detail.component';
 import { StarComponent } from './shared/ui/star/star.component';
+import { ProductSearchPipe } from './product/product-search.pipe';
 
 @NgModule({
   declarations: [
@@ -16,10 +20,12 @@ import { StarComponent } from './shared/ui/star/star.component';
     WelcomeComponent,
     ProductListComponent,
     ProductDetailComponent,
-    StarComponent
+    StarComponent,
+    ProductSearchPipe
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+
+  constructor() {}
 
   ngOnInit() {
+   const pageTitle: string = 'gestion de stock ';
   }
 
 }
